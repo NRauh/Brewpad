73e561b23897a91786550cfad931a3af2dba7147
Nate Rauh <nrauh@users.noreply.github.com>
2018-09-21 19:35:06 -0500

    Add material and pages
    Added vue material and placeholder pages for the stuff.

--- projects/ui/src/App.vue
+++ projects/ui/src/App.vue
@@ -1,8 +1,38 @@
 <template>
-  <div id="app">
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <span class="md-title">Brewpad</span>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row">
+          <md-tabs class="md-primary">
+            <md-tab
+              id="tab-brews"
+              md-label="Brews"
+              to="brews"
+            />
+            <md-tab
+              id="tab-recipes"
+              md-label="Recipes"
+              to="recipes"
+            />
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-content>
+        <router-view />
+      </md-app-content>
+    </md-app>
+  </div>
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
     </div>
-    <router-view/>
-  </div>
+  </div> -->
+
 </template>
---
+++ projects/ui/src/views/Recipes.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Here are you recipes</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recipes',
+};
+</script>
